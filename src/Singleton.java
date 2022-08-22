46ab8af55553066851b5f51437e0231e925d18ce
1552845373 <1552845373@qq.com>
2022-08-22 23:09:35 +0800

    update

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,8 +1,54 @@
-// 饿汉式
-public class Singleton {
-    private static Singleton singleton = new Singleton();
-    private Singleton() {}
-    public static Singleton getInstance() {
-        return singleton;
+//// 饿汉式
+//public class Singleton {
+//    private static Singleton singleton = new Singleton();
+//    private Singleton() {}
+//    public static Singleton getInstance() {
+//        return singleton;
+//    }
+//}
+
+//// 懒汉式
+//public class Singleton {
+//    private static Singleton singleton;
+//    private Singleton() {}
+//    public static synchronized Singleton getInstance() {
+//        if (singleton == null) {
+//            singleton = new Singleton();
+//        }
+//        return singleton;
+//    }
+//}
+
+//// 静态内部类
+//public class Singleton {
+//    private static class SingletonHolder {
+//        private static final Singleton INSTANCE = new Singleton();
+//    }
+//    private Singleton() {}
+//    public static Singleton getInstance() {
+//        return SingletonHolder.INSTANCE;
+//    }
+//}
+
+//// 双重校验锁
+//public class Singleton {
+//    private static Singleton singleton;
+//    private Singleton() {}
+//    private static Singleton getInstance() {
+//        if (singleton == null) {
+//            synchronized (Singleton.class) {
+//                if (singleton == null) {
+//                    singleton = new Singleton();
+//                }
+//            }
+//        }
+//        return singleton;
+//    }
+//}
+
+// 枚举
+public enum Singleton {
+    INSTANCE;
+    public void whateverMethod() {
     }
 }
