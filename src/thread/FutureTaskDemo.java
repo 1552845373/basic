06720c4680f4b1de2de42680d3a71f09a4e6d039
1552845373 <1552845373@qq.com>
2022-08-22 21:50:48 +0800

    update

--- src/thread/FutureTaskDemo.java
+++ src/thread/FutureTaskDemo.java
@@ -1,12 +1,32 @@
 package thread;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class FutureTaskDemo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        FutureTask<String> task = new FutureTask<>(new Callable<String>() {
+//        FutureTask<String> task = new FutureTask<>(new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                String value = "is Done";
+//                Thread.sleep(2000);
+//                return value;
+//            }
+//        });
+//
+//        new Thread(task).start();
+//        while (!task.isDone()) {
+//            System.out.println("please wait");
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println(task.get());
+
+        // 线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> future = executorService.submit(new Callable<String>() {
             @Override
             public String call() throws Exception {
                 String value = "is Done";
@@ -15,15 +35,16 @@ public class FutureTaskDemo {
             }
         });
 
-        new Thread(task).start();
-        while (!task.isDone()) {
-            System.out.println("please wait");
+        if (!future.isDone()) {
             try {
-                Thread.sleep(500);
+                System.out.println(future.get());
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                executorService.shutdown();
             }
         }
-        System.out.println(task.get());
     }
 }
